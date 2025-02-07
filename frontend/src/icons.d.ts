5053583f09c1c2b32b0e575454c4aace5c02e67b
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-02-07 20:42:37 +0100

    deploy all

---
+++ frontend/src/icons.d.ts
@@ -0,0 +1,9 @@
+declare module '@fortawesome/free-brands-svg-icons' {
+  export const faDeezer: any;
+  // Add other icons as needed
+}
+
+declare module '@fortawesome/free-solid-svg-icons' {
+  export const faCircleMinus: any;
+  export const faLightbulb: any;
+}
